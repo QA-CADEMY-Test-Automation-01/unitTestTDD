26a27d715951f460144ed1e9a8d33b7f6cda3bb7
Bruno Barrios <bbarrios@number8.com>
2020-09-02 10:03:03 -0400

    Added unit test created on class 3

--- src/main/java/qacademy/katas/Kata.java
+++ src/main/java/qacademy/katas/Kata.java
@@ -12,4 +12,57 @@ public class Kata {
     public static String createPhoneNumber2(int[] numbers) {
         return String.format("(%d%d%d) %d%d%d-%d%d%d%d", java.util.stream.IntStream.of(numbers).boxed().toArray()); // Second solution
     }
+
+    public static String maxMinString(String numbers) {
+        int mayor;
+        int menor;
+        String[] arreglo = (numbers.split(" "));
+        mayor = Integer.parseInt(arreglo[0]);
+        menor = mayor;
+        for (String numberN : arreglo) {
+
+            if (numberN.isEmpty()) {
+                continue;
+            }
+
+            int num1 = Integer.parseInt(numberN);
+            if (mayor < num1) mayor = num1;
+            if (menor > num1) menor = num1;
+        }
+        return "".concat(String.valueOf(mayor)).concat(" ").concat(String.valueOf(menor));
+    }
+
+    public static String camelCase(String word) {
+        if (word.isEmpty()) {
+            return word;
+        }
+        if (word.length() == 1 && !word.isBlank()) {
+            return word.toUpperCase();
+        }
+        StringBuilder resultCamelCase = new StringBuilder();
+        for (String s : word.strip().split(" ")) {
+            if (s.isEmpty()) {
+                continue;
+            }
+            resultCamelCase.append(s.substring(0, 1).toUpperCase().concat(s.substring(1)));
+        }
+        return resultCamelCase.toString();
+    }
+
+    public static int multiplos3y5(int numero) {
+        int sum = 0;
+        for (int i = 0; i < numero; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                System.out.println(i);
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    public static String getMiddle(String a) {
+        int lengthA = a.length();
+        int half = lengthA / 2;
+        return lengthA % 2 == 0 ? a.substring(half - 1, half + 1) : a.substring(half, half + 1);
+    }
 }
--- src/test/java/qacademy/katas/KataTest.java
+++ src/test/java/qacademy/katas/KataTest.java
@@ -11,4 +11,37 @@ public class KataTest {
         assertEquals("(123) 456-7890", Kata.createPhoneNumber1(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
         assertEquals("(123) 456-7890", Kata.createPhoneNumber2(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
     }
+
+    @Test
+    public void testNumberMaxMin() {
+        assertEquals("42 -9", Kata.maxMinString("8 3 -5 42  -1 0 0 -9 4 7 4 -4"));
+
+
+    }
+
+    @Test
+    public void testTwoWords() {
+        assertEquals("TestCase", Kata.camelCase("test case"));
+        assertEquals("CamelCaseMethod", Kata.camelCase("camel case method"));
+        assertEquals("CamelCaseWord", Kata.camelCase(" camel case word"));
+        assertEquals("SayHello", Kata.camelCase("say hello "));
+        assertEquals("AbC", Kata.camelCase("ab  c"));
+        assertEquals("Z", Kata.camelCase("z"));
+        assertEquals("", Kata.camelCase(""));
+        assertEquals("", Kata.camelCase(" "));
+    }
+
+
+    @Test
+    public void test() {
+        assertEquals(23, Kata.multiplos3y5(10));
+    }
+
+    @Test
+    public void evenTests() {
+        assertEquals("es", Kata.getMiddle("test"));
+        assertEquals("dd", Kata.getMiddle("middle"));
+        assertEquals("t", Kata.getMiddle("testing"));
+        assertEquals("A", Kata.getMiddle("A"));
+    }
 }
